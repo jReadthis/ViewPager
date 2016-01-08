72c6fcedee07ed4af2e5042c7a8d2926a8522167
Juan <O.marca25@gmail.com>
2016-01-07 20:23:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nano1/viewpager/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nano1.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
+}
+
